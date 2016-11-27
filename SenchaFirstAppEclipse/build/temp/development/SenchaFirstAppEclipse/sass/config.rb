2ae48ab4076a08c68acbf414ee349708cbde66fc
AK <ankit.kumar02@outlook.com>
2016-11-27 17:08:55 +0530

    This is my first commit. Just wrote some comments in index.html

---
+++ SenchaFirstAppEclipse/build/temp/development/SenchaFirstAppEclipse/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\Users\IBM_ADMIN\Desktop\LearningExtJS\SenchaFirstAppEclipse/build/.sass-cache'
---
+++ SenchaFirstAppEclipse/build/temp/development/SenchaFirstAppEclipse/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\IBM_ADMIN\Desktop\LearningExtJS\SenchaFirstAppEclipse\app.js
